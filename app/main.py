c62747d2677180c2f3327b8a13f0f92d092aa1c6
cooper <gluhovkich@gmail.com>
2022-01-01 01:54:58 +0500

    finish the main part of the frontend site
    Add fieled where will be the resulting excerpt.
    Add and connect js.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -10,6 +10,19 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 def home_page(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    message = """
 
+— Смогла ли я поселиться в твоём сердце?
+— Да ты вломилась в него не снимая обуви..
+
+Аракава Наоси "Твоя апрельская ложь"
+"""
+    return templates.TemplateResponse("home.html", {"request": request, "message": message})
+
+
+@app.post("/test")
+def test_page(message: str = Form(...)):
+    message_test = message
+    print(message_test)
+    return {"message": message_test}
 
