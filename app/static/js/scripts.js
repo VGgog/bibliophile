186300ba126a57171287cd7b826362fb979e0ea3
cooper <gluhovkich@gmail.com>
2022-01-03 00:20:36 +0500

    implemented getting data from textarea

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,1 +1,11 @@
-alert("Hello World");
+window.onload = function (){
+    var submit = document.getElementById('submit');
+    submit.addEventListener('click', handler);
+
+    function handler() {
+        var data = document.getElementById('text-fragment').value;
+        if (data){
+        alert(data);
+    }
+    }
+}
