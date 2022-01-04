b8cf99cdd2e90a503e1cf81eace3ad09fdac7c29
cooper <gluhovkich@gmail.com>
2022-01-05 01:02:25 +0500

    Add getting phrase and send response on frontend

--- app/main.py
+++ app/main.py
@@ -1,28 +1,27 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
+class PhraseJSON(BaseModel):
+    phrase: str
+
+
 @app.get("/", response_class=HTMLResponse)
 def home_page(request: Request):
-    message = """
-
-— Смогла ли я поселиться в твоём сердце?
-— Да ты вломилась в него не снимая обуви..
-
-Аракава Наоси "Твоя апрельская ложь"
-"""
-    return templates.TemplateResponse("home.html", {"request": request, "message": message})
+    """Return website home page."""
+    return templates.TemplateResponse("home.html", {"request": request})
 
 
-@app.post("/test")
-def test_page(message: str = Form(...)):
-    message_test = message
-    print(message_test)
-    return {"message": message_test}
-
+@app.post("/")
+def returning_the_found_passage(phrase_obj: PhraseJSON):
+    """Function for getting a phrase and return the found passage."""
+    fragment_text = phrase_obj.phrase
+    return {"fragment_text": fragment_text}
