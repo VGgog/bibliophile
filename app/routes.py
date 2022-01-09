8da594bed2e11f71fed3224c9386f6d791b9a1e5
cooper <gluhovkich@gmail.com>
2022-01-10 00:25:27 +0500

    Add serch fragment in text
    Add new module in which a searching is being performed fragment

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from fastapi.responses import HTMLResponse
 from app import app, templates
 from fastapi import Request
 from app import validation
+from app import fragment
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -13,5 +14,6 @@ def home_page(request: Request):
 @app.post("/")
 def returning_the_found_passage(phrase_obj: validation.PhraseJSON):
     """Function for getting a phrase and return the found passage."""
-    fragment_text = phrase_obj.phrase
+    phrase = phrase_obj.phrase
+    fragment_text = fragment.return_fragment(phrase)
     return {"fragment_text": fragment_text}
