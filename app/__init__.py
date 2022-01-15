32e3021e243666063e7a60dbe09a27e71aca3026
Aidar <gluhovkich@gmail.com>
2022-01-16 01:27:04 +0500

    Add admin package

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,9 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-
 app = FastAPI()
 app.mount("/app/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
-
 from app import routes
+from app.admin import route
