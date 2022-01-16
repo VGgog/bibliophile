4c302d7f9ba2e1953ecb17a9e894c45a6afcc665
Aidar <gluhovkich@gmail.com>
2022-01-16 17:17:09 +0500

    Add route for add new url for book in db
    This function add url in db.
    Url for different books where from will be parce text fragment.

--- app/admin/route.py
+++ app/admin/route.py
@@ -3,7 +3,22 @@ from app.admin import db_crud
 from app.admin import models
 from app.admin import schema
 from app.admin.database import engine, session
+from fastapi import Depends
+from sqlalchemy.orm import Session
 
 
 models.base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/admin/add")
+def add_url_in_db(url_data: schema.URLModel, db: Session = Depends(get_db)):
+    return db_crud.add_url(db=db, url_data=url_data)
 
