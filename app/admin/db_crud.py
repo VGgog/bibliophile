42b06c458e68969ac0a632cfe3f00b3e6a297c2e
Aidar <gluhovkich@gmail.com>
2022-01-16 17:08:27 +0500

    Fix typo

--- app/admin/db_crud.py
+++ app/admin/db_crud.py
@@ -5,7 +5,7 @@ from app.admin import schema
 
 def add_url(db: Session, url_data: schema.URLModel):
     data = models.Urls(url=url_data.url, author=url_data.author, 
-            book_tile=url_data.book_title, max_page=url_data.max_page)
+            book_title=url_data.book_title, max_page=url_data.max_page)
     db.add(data)
     db.commit()
     db.refresh(data)
