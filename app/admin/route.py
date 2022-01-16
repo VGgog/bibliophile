b32c2fc29fe4e117067e4774e2670d00f3e0e6bd
Aidar <gluhovkich@gmail.com>
2022-01-16 22:29:28 +0500

    Create token verification
    Token verification so that no one except the admin can add new urls.

--- app/admin/route.py
+++ app/admin/route.py
@@ -5,6 +5,7 @@ from app.admin import schema
 from app.admin.database import engine, session
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
+from config import Config
 
 
 models.base.metadata.create_all(bind=engine)
@@ -20,7 +21,12 @@ def get_db():
 
 @app.post("/admin/add")
 def add_url_in_db(url_data: schema.URLModel, db: Session = Depends(get_db)):
+    
+    if not url_data.token == Config.admin_token:
+        raise HTTPException(status_code=403, detail="You don't have rights")
+
     if db_crud.check_url_in_db(db=db, url=url_data.url):
         raise HTTPException(status_code=400, detail="URL already added")
+    
     return db_crud.add_url(db=db, url_data=url_data)
 
