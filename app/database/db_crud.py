5bc544ce79607d4fe7c56f281154800718e0e3b0
Aidar <gluhovkich@gmail.com>
2022-01-19 23:13:57 +0500

    Create function which return number of books in db
    This function need in fragment.py model for right request for text

--- app/database/db_crud.py
+++ app/database/db_crud.py
@@ -17,3 +17,9 @@ def add_book_in_db(db: Session, data: schema.Book):
 def check_book_in_db(db: Session, book_title):
     """Check availability of the book(text-url, author, book title etc.) in db"""
     return db.query(models.Book).filter(models.Book.book_title == book_title).first()
+
+
+def number_of_books(db: Session):
+    """return number of books in db"""
+    return db.query(models.Book).count()
+
