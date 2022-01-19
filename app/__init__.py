962c57e012fc15ba8d3581d89389b506dd0e9aa4
Aidar <gluhovkich@gmail.com>
2022-01-19 23:15:33 +0500

    Add migration

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from app.admin import models
+from app.database import base, engine
+
+
 app = FastAPI()
 app.mount("/app/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
+
+models.base.metadata.create_all(bind=engine)
 
 from app import routes
 from app.admin import route
