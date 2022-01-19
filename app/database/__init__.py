19bcfe287271e2ed948b21e4946ed38e9f3867f9
Aidar <gluhovkich@gmail.com>
2022-01-19 23:06:47 +0500

    Create get_db for depency in db

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -6,3 +6,13 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine("postgresql+psycopg2://postgres:monoliza@localhost/bibliophile")
 session = sessionmaker(engine)
 base = declarative_base()
+
+
+def get_db():
+    """Create dependency"""
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
