d3918433510c5fa86157c37857cebb75fbb49bcd
Aidar <gluhovkich@gmail.com>
2022-01-25 01:07:58 +0500

    Create asynchronous requests for get html text
    This way the search will be performed faster.

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
+import random
+
 from fastapi.responses import HTMLResponse
-from app import app, templates
+import aiohttp
 from fastapi import Request
+
+from app import app, templates
 from app import validation
 from app import fragment
-
-from app.database import session
+from app.database import session, db_crud
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -17,24 +20,35 @@ async def home_page(request: Request):
 async def returning_the_found_passage(phrase_obj: validation.PhraseJSON):
     """Function for getting a phrase and return the found passage."""
     phrase = phrase_obj.phrase
-    result = searching(phrase)
+    result = await searching(phrase)
     return {"fragment_text": result}
 
 
-def searching(phrase):
+async def searching(phrase):
     """Searching fragment."""
     fragment_text = None
     step = 0
-    while not fragment_text and step < 25:
-        try:
-            db = session()
-            book = fragment.GetFragment(phrase, db)
-        finally:
-            db.close()
-        fragment_text = book.result
-        step += 1
+    async with aiohttp.ClientSession() as cl_session:
+        while not fragment_text and step < 25:
+            try:
+                db = session()
+                book = db_crud.return_book(db, random.randrange(1, db_crud.number_of_books(db)+1))
 
-    if fragment_text:
-        return fragment_text
-    else:
-        return "Отрывок не найден..."
+                if book.max_page != 1:
+                    url = book.url[:-11] + "/p." + str(random.randrange(1, book.max_page + 1)) + book.url[-11:]
+                else:
+                    url = book.url[:-11] + "/p.1" + book.url[-11:]
+
+                async with cl_session.get(url) as response:
+                    html_text = await response.text()
+                    book = fragment.GetFragment(phrase, db, html_text)
+
+            finally:
+                db.close()
+            fragment_text = book.result
+            step += 1
+
+        if fragment_text:
+            return fragment_text
+        else:
+            return "Отрывок не найден..."
