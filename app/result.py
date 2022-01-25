54055a3464c5fb98d7bb240e0519de19dda7347e
Aidar <gluhovkich@gmail.com>
2022-01-26 00:46:13 +0500

    Refactor code and broke the odea function into several

--- app/result.py
+++ app/result.py
@@ -1,16 +1,17 @@
 import aiohttp
 from app.database import session
-from app import fragment
+from app import searching
 
 
 async def get_result(phrase):
     """Return result of searching fragment"""
     async with aiohttp.ClientSession() as client:
+        # Bibliophile search a fragment in 100 book text.
         for i in range(100):
             with session() as db:
 
-                book = fragment.get_book(db)
-                result = await get_result_of_searching(client, phrase, book)
+                book = searching.get_book(db)
+                result = await get_the_search_result(client, phrase, book)
                 
                 if result:
                     return result
@@ -18,20 +19,20 @@ async def get_result(phrase):
         return "Отрывок не найден..."
 
 
+async def get_the_search_result(client, phrase, book):
+    """Return fragment if fragment found else none"""
+
+    url = searching.get_text_link(book)
+
+    html_text = await send_request(client, url)
+                
+    text = searching.get_text(html_text)
+    result = searching.search_fragment_in_text(phrase, text)
+     
+    return result
+
+
 async def send_request(client, url):
     """Send get request on web-site with text"""
     async with client.get(url) as response:
         return await response.text()
-
-
-async def get_result_of_searching(client, phrase, book):
-    """Return fragment if fragment found else none"""
-
-    url = fragment.get_text_link(book)
-
-    html_text = await send_request(client, url)
-                
-    text = fragment.get_text(html_text)
-    result = fragment.search_fragment_in_text(phrase, text)
-     
-    return result
---
+++ app/searching.py
@@ -0,0 +1,55 @@
+import re
+import random
+
+from bs4 import BeautifulSoup
+
+from app.database import db_crud
+
+
+def get_book(db):
+    """Get random book in db"""
+    return db_crud.return_book(db=db, book_id=random.randrange(1, db_crud.number_of_books(db)+1))
+
+
+def get_text_link(book):
+    """
+    Return text_link.
+    example: book_url = https://ilibrary.ru/text/11/index.html
+    But text located at the link https://ilibrary.ru/text/11/p.{number of page}/index.html.
+    Due to this function converts book_url to the text_link when text located.
+    Number of page = random number before max_page
+    book.url[:-11] = https://ilibrary.ru/text/11
+    book.url[-11:] = /index.html
+    """
+    if book.max_page != 1:
+        return book.url[:-11] + "/p." + str(random.randrange(1, book.max_page + 1)) + book.url[-11:]
+    else:
+        return book.url[:-11] + "/p.1" + book.url[-11:]
+
+
+def get_text(html_text):
+    """Return text of the book"""
+    pars_obj = BeautifulSoup(html_text, 'lxml')
+    text = pars_obj.find('div', id='text')
+    return text.text
+
+
+def search_fragment_in_text(phrase, text):
+    """
+    Search need phrase in text.
+    If fragment was found return fragment.
+    Else return None.
+    """
+    result = re.search("\n.*"+phrase+".*\n", text)
+    if result:
+        fragment = result[0].strip()
+        # If fragment close ":"(in russian language most often it means that text ends with a direct speech),
+        # that take next paragraph
+        if fragment[-1] == ":":
+            fragment = re.search("\n.*"+phrase+".*\n.*\n", text)
+            fragment = fragment[0].strip()
+            return fragment
+        else:
+            return fragment
+
+    return None
