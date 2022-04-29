cbed4b776dad3619d74a8603e3a6b5da2b805eef
Aidar <gluhovkich@gmail.com>
2022-04-30 01:41:05 +0500

    New migration

---
+++ migrations/versions/557749ad82cb_delete_column_url_and_add_text_column.py
@@ -0,0 +1,24 @@
+"""Delete column url and add text column
+
+Revision ID: 557749ad82cb
+Revises: 6ef0812636d4
+Create Date: 2022-04-29 01:12:21.970967
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '557749ad82cb'
+down_revision = '6ef0812636d4'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
